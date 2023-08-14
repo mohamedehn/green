525573f49c3807dc0235f2c884211440d96f0b24
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2023-08-15 00:18:51 +0200

    Starting project

---
+++ src/app/cookies/page.js
@@ -0,0 +1,8 @@
+
+export default function Mentions(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/app/mentions/page.js
@@ -0,0 +1,8 @@
+
+export default function Cookies(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
