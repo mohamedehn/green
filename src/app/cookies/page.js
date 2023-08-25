bbeafb8b112c7cf7bb2323682f40f7c3b74d28a7
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2023-08-25 19:45:33 +0200

    Cookies and Legal notice pages done

--- src/app/cookies/page.js
+++ src/app/cookies/page.js
@@ -1,8 +1,105 @@
+'use client'
+import React from "react"
+import Header from "../components/header"
+import Footer from "../components/footer"
 
-export default function Mentions(){
+export default function Cookies(){
     return(
         <div>
-            
+        <Header/>
+        <div className="bg-greyforbg py-20 sm:py-24">
+            <div className="mx-auto max-w-6xl px-6 lg:px-8 bg-white rounded-xl py-16">
+                <div className="mx-auto lg:mx-0">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">POLITIQUE DE GESTION DES COOKIES</h2>
+                    <h3 className="text-xl font-bold tracking-tight text-gray-900 sm:text-2xl pt-10">INFORMATIONS À PROPOS DES COOKIES</h3>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Lors de la consultation de notre site internet, des cookies sont déposés sur le terminal (ordinateur, tablette, smartphone) 
+                        avec lequel vous naviguez.
+                    </p>
+                    <h3 className="text-xl font-bold tracking-tight text-gray-900 sm:text-2xl pt-10">QU’EST CE QU’UN COOKIE ?</h3>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Le cookie est un petit fichier texte déposé dans un espace dédié du disque dur de votre terminal qui va stocker des 
+                        informations relatives à votre navigation. Il est téléchargé par votre navigateur lors de la visite d’un site internet. 
+                        Lorsque vous revenez sur ce site en utilisant le même appareil, le cookie et les informations qui y sont stockés sont 
+                        soit envoyés au site internet qui l’a créé (cookie propriétaire) soit envoyés à un autre site internet auquel il 
+                        appartient (cookie tiers). Certains cookies sont extrêmement utiles car ils peuvent améliorer l’expérience de 
+                        l’utilisateur. Par exemple, lorsque vous consultez plusieurs fois un site internet avec un même navigateur, les cookies 
+                        gardent en mémoire, vos préférences, la manière dont vous utilisez un site et adaptent le contenu affiché pour qu’il soit 
+                        adapté à vos besoins et intérêts personnels. Selon leur fonction et leur finalité, les cookies peuvent être divisés en 
+                        quatre catégories : les cookies essentiels, les cookies de mesures d’audience et de statistique, les cookies fonctionnels 
+                        et les cookies publicitaires.
+                    </p>
+                    <h3 className="text-xl font-bold tracking-tight text-gray-900 sm:text-2xl pt-10">QUELLE EST LA DUREE DE CONSERVATION D’UN COOKIE ?</h3>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Les cookies déposés par Green Energy Solutions, nos sous-traitants et nos partenaires et les données de navigations 
+                        recueillies sont conservées pour une durée maximale de 13 mois à compter du dépôt sur votre terminal. A l’expiration de 
+                        ce délai, votre consentement sera à nouveau nécessaire au dépôt du cookie.
+                    </p>
+                    <h3 className="text-xl font-bold tracking-tight text-gray-900 sm:text-2xl pt-10">LES CATEGORIES DE COOKIES</h3>
+                    <h4 className="text-lg font-semibold tracking-tight text-gray-900 sm:text-xl pt-6">LES COOKIES ESSENTIELS :</h4>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Les cookies essentiels, également appelés cookies « strictement nécessaires », assurent des fonctions sans lesquelles 
+                        le site ne peut pas fonctionner correctement. Ils sont stockés sur votre appareil uniquement pendant la durée de la 
+                        session d’utilisation du navigateur, et votre consentement n’est pas requis pour ce type de cookie.
+                    </p>
+                    <h4 className="text-lg font-semibold tracking-tight text-gray-900 sm:text-xl pt-6">LES COOKIES DE MESURES D’AUDIENCE ET DE STATISTIQUES :</h4>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Les cookies de mesures d’audience et de statistique permettent de produire des statistiques anonymes quant à la 
+                        fréquentation du site (ex : nombre de visites, les pages consultées, le temps passé…). Ces cookies permettent d’adapter 
+                        le site aux demandes des visiteurs et d’améliorer sa performance et ses fonctionnalités.
+                    </p>
+                    <h4 className="text-lg font-semibold tracking-tight text-gray-900 sm:text-xl pt-6">LES COOKIES DE FONCTIONNELS :</h4>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Les cookies fonctionnels permettent à un site internet d’enregistrer des informations qui ont déjà été fournies 
+                        (ex : le nom de l’utilisateur, la localisation) et d’offrir à l’utilisateur des fonctions améliorées et personnalisées.
+                    </p>
+                    <h4 className="text-lg font-semibold tracking-tight text-gray-900 sm:text-xl pt-6">LES COOKIES PUBLICITAIRES :</h4>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Les cookies publicitaires sont utilisés pour diffuser des publicités qui sont pertinentes pour certains utilisateurs 
+                        spécifiques et adaptées à leurs intérêts.
+                    </p>
+                    <h3 className="text-xl font-bold tracking-tight text-gray-900 sm:text-2xl pt-10">COMMENT GÉRER ET SUPPRIMER LES COOKIES ?</h3>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Lors de votre première visite sur le site, un bandeau vous informe de la présence de ces cookies et vous invite à les 
+                        accepter ou les refuser. Les cookies, qui nécessitent votre consentement, seront déposés uniquement si vous y consentez.
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Par ailleurs, pour gérer les cookies, la plupart des navigateurs vous permettent de refuser ou d’accepter tous les 
+                        cookies ou de n’en accepter que certains. Vous trouverez la procédure de gestion et de suppression des cookies à la 
+                        rubrique aide de votre navigateur, ou directement en cliquant sur les liens suivants :
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Pour Chrome, cliquer&nbsp;
+                        <a href="http://support.google.com/chrome/bin/answer.py?hl=fr&hlrm=en&answer=95647">ici</a>
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Pour Safari, cliquer&nbsp;
+                        <a href="http://support.apple.com/kb/HT1677?viewlocale=fr_FR">ici</a>
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Pour Firefox, cliquer&nbsp;
+                        <a href="http://support.mozilla.org/fr/kb/activer-desactiver-cookies?redirectlocale=fr&redirectslug=Activer+et+d%C3%A9sactiver+les+cookies">
+                            ici
+                        </a>
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Pour Opera, cliquer&nbsp;
+                        <a href="http://help.opera.com/Windows/10.20/fr/cookies.html">ici</a>
+                    </p>
+                    <h3 className="text-xl font-bold tracking-tight text-gray-900 sm:text-2xl pt-10">
+                        MISES À JOUR DE LA POLITIQUE DE GESTION DES COOKIES :
+                    </h3>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Green Energy Solutions se réserve le droit de modifier ou de mettre à jour cette politique lorsque cela devient 
+                        nécessaire (dernière mise à jour le 09/08/2023), afin de s’adapter aux retours clients, aux changements liés à nos 
+                        produits et services, ou pour être conforme à de nouvelles exigences légales. Nous vous prions de bien vouloir la 
+                        consulter régulièrement. Lorsque nous mettons à jour cette politique, nous révisons la date de « dernière mise à jour » en 
+                        haut de ce document.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <Footer/>
         </div>
     )
 }
--- src/app/mentions/page.js
+++ src/app/mentions/page.js
@@ -1,8 +1,89 @@
+'use client'
 
-export default function Cookies(){
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+export default function Mentions(){
     return(
         <div>
-            
+        <Header/>
+        <div className="bg-greyforbg py-20 sm:py-24">
+            <div className="mx-auto max-w-6xl px-6 lg:px-8 bg-white rounded-xl py-16">
+                <div className="mx-auto lg:mx-0">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Mentions légales</h2>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                    Le présent site est la propriété de Green Energy Solutions <br/>
+                    Siège social : 40 Rue de Bruxelles - 69100 VILLEURBANNE - Email: contact@greenenergysolutions.fr <br/>
+                    Entreprise créée en 2022 – code 4322B - Convention BTP et construction - SIRET : 91416308400017 <br/>
+                    Toute reproduction même partielle est interdite sans autorisation préalable. <br/>
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                    2 - PRESTATAIRES <br/>
+                    Création / Conception / Développement : <br/>
+                    Nom de l&apos;entreprise : Coding Corp <br/>
+                    SIRET : 953 263 423 00014 <br/>
+                    Email : contact@codingcorp.fr - Web : https://www.codingcorp.fr <br/>
+                    Hébergement : Hostinger <br/>
+                    </p>
+                    <h2 className="pt-20 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Politique de confidentialité</h2>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Les internautes accédant au site https://greenenergysolutions.fr/ peuvent être amenés à fournir des informations nominatives 
+                        ou à caractère personnel dans le cadre des services proposés par Green Energy Solutions.
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        L’entreprise Green Energy Solutions est responsable de traitement au sens du Règlement (UE) 2016/679 dit Règlement 
+                        Général pour la Protection des Données ou « RGPD », pour la collecte et le traitement des données à caractère personnel 
+                        effectuées sur le site accessible à l’adresse URL : https://greenenergysolutions.fr/
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        L’entreprise Green Energy Solutions traite les données personnelles collectées via son site pour les finalités suivantes :
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                    - Afin de permettre au visiteur du site d’obtenir des informations sur les services proposés par l&apos;entreprise Green Energy 
+                    Solutions et les conditions d’éligibilité. <br/>
+                    Conformément au RGPD, le visiteur du site dispose sur ses données personnelles des droits suivants :<br/>
+                    - Un droit d’accès <br/>
+                    - Un droit de rectification <br/>
+                    - Un droit d’effacement <br/>
+                    - Le cas échéant, d’un droit à la portabilité de ses données <br/>
+                    - Un droit de demander la limitation du traitement de ses données et/ ou de s’y opposer <br/>
+                    - Le cas échéant, un droit de retirer son consentement <br/>
+                    - Un droit de définir des directives relatives au sort de ses données après sa mort <br/>
+                    Le visiteur peut exercer ses droits en envoyant soit : <br/>
+                    - Un courriel au Responsable de Traitement de Green Energy Solutions <br/>
+                    - Un courrier à l’attention du Responsable de Traitement à l’adresse suivante : <br/>
+                    Green Energy Solutions <br/>
+                    40 Rue de Bruxelles <br/>
+                    69100 VILLEURBANNE <br/>
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Si le visiteur estime que Green Energy Solutions n’a pas correctement répondu à ses sollicitations, il dispose d’un droit 
+                        d’introduire une réclamation auprès de la CNIL. Green Energy Solutions limite la collecte des données personnelles au 
+                        strict nécessaire au regard des finalités des traitements de données, elles sont destinées à l’entreprise Green Energy 
+                        Solutions mais sont susceptibles d’être transférées à des organismes publics afin de mieux répondre à la demande du visiteur.
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Les données ne sont en aucun cas communiquées, ni vendues, ni cédées, louées et / ou commercialisées à des tiers au sens 
+                        du RGPD.
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Les données sont stockées exclusivement sur le territoire Français et ne font l’objet d’aucun transfert en dehors de l’Union 
+                        Européenne.
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Les données seront conservées tant que le visiteur souhaite recevoir des informations, et le temps nécessaire à la gestion de 
+                        sa demande.
+                    </p>
+                    <p className="mt-6 text-base leading-8 text-gray-600 text-justify">
+                        Green Energy Solutions veille à ce que toutes les mesures techniques et organisationnelles afin de protéger les données 
+                        personnelles soient mises en œuvre, ces règles de confidentialités et de sécurité sont également imposées à l’ensemble du 
+                        personnel de Green Energy Solutions, ainsi qu’à tous les prestataires et sous-traitants travaillant pour Green Energy 
+                        Solutions.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <Footer/>
         </div>
     )
 }
